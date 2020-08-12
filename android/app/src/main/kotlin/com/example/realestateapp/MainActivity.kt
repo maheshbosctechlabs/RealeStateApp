d5431a0f3b3fe709ed55f975da59b16775589ca8
Kalpesh <kalpesh.khandla@bosc.in>
2020-08-12 18:34:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/realestateapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.realestateapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:realestateapp/home_page_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePageScreen(),
+    );
+  }
+}
